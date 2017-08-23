9dfbb9339031bc12a984b463c8bc1fa603e9130d
nickkolok <nickkolok@mail.ru>
2017-08-23 14:27:52 +0300

    Error throwing when test failing

--- tests/forbid-console/main.js
+++ tests/forbid-console/main.js
@@ -1,7 +1,11 @@
 var paraquire = require("paraquire")(module);
+var accessed = false;
 try{
     var f = paraquire("./lib-with-console.js");
     f();
+    accessed = true;
 }catch(e){
-    console.log(e);
+}
+if (accessed){
+    throw new Error('Able to access forbidden "console"');
 }
--- tests/forbid-process/main.js
+++ tests/forbid-process/main.js
@@ -1,7 +1,12 @@
 var paraquire = require("paraquire")(module);
+
+var accessed = false;
 try{
     var f = paraquire("./lib-with-process.js");
     console.log(f());
+    accessed = true;
 }catch(e){
-    console.log(e);
+}
+if (accessed){
+    throw new Error('Able to access forbidden "process"');
 }
