e0db274e4c1491616dfc318f361559998d868685
nickkolok <nickkolok@mail.ru>
2017-08-23 15:00:06 +0300

    Test for access to "process"

--- tests/forbid-process/main.js
+++ tests/forbid-process/main.js
@@ -10,3 +10,25 @@ try{
 if (accessed){
     throw new Error('Able to access forbidden "process"');
 }
+
+accessed = false;
+try{
+    f = paraquire("./lib-with-process.js",{sandbox:{process:{argv:process.argv,env:{LC_ALL:0}}}});
+    console.log(f());
+    accessed = true;
+}catch(e){
+}
+if (!accessed){
+    throw new Error('Unable to access permitted "process.argv"');
+}
+
+accessed = false;
+try{
+    f = paraquire("./lib-with-process.js",{sandbox:{process:process}});
+    console.log(f());
+    accessed = true;
+}catch(e){
+}
+if (!accessed){
+    throw new Error('Unable to access permitted "process"');
+}
