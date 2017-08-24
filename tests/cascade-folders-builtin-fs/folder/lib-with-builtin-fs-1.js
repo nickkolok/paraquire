97cd828539a5ff0ebfe3110989fc5f3dcb9711de
nickkolok <nickkolok@mail.ru>
2017-08-24 10:51:46 +0300

    Tests for cascade requiring in different folders

---
+++ tests/cascade-folders-builtin-fs/folder/lib-with-builtin-fs-1.js
@@ -0,0 +1,3 @@
+module.exports = function(){
+    return require('../lib-with-builtin-fs-final.js');
+}
--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,5 @@
 require('./manage-builtin-fs/main.js');
 require('./cascade-builtin-fs/main.js');
+require('./cascade-folders-builtin-fs/main.js');
 require('./forbid-process/main.js');
 require('./forbid-console/main.js');
