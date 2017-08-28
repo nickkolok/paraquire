0afd2033deae40e66939ffbe55b4b2452116fcb0
nickkolok <nickkolok@mail.ru>
2017-08-28 12:02:12 +0300

    Excluded test about stack in NodeJS <=0.10

--- tests/callsite-stack/main.js
+++ tests/callsite-stack/main.js
@@ -3,6 +3,16 @@ var paraquire = require("paraquire")(module);
 var f;
 
 
+if (process && process.version) {
+    var parts = process.version.split('.');
+    if (parts[0] === 'v0' && (1*parts[1]) <= 10) {
+        console.log('/**********************  WARNING  ***********************/');
+        console.log('/* Your NodeJS is too old, paraquire cannot protect it. */');
+        console.log('/* Your application will run without full protection    */');
+        console.log('/********************************************************/');
+        return;
+    }
+}
 
 
 var accessed = false;
